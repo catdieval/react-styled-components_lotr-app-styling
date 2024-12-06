c9e6bc45ba8f5864eb9e48d0a772d89de3f0ed47
Catherine <catherine.dieval@hotmail.fr>
2024-12-06 10:39:20 +0100

    Styled Link in volumes/index.js

--- pages/volumes/index.js
+++ pages/volumes/index.js
@@ -1,5 +1,14 @@
 import Link from "next/link";
 import { introduction, volumes } from "../../lib/data";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  box-shadow: var(--box-shadow-book);
+
+  &:hover {
+    box-shadow: var(--box-shadow-book--hover);
+  }
+`;
 
 export default function Volumes() {
   return (
@@ -9,7 +18,7 @@ export default function Volumes() {
       <ul>
         {volumes.map((volume) => (
           <li key={volume.id}>
-            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+            <StyledLink href={`/volumes/${volume.slug}`}>{volume.title}</StyledLink>
           </li>
         ))}
       </ul>
