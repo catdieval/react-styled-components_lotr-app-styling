cd0f74a2826bdb97295edbf766a9a47cb7f9f208
Catherine <catherine.dieval@hotmail.fr>
2024-12-17 12:19:15 +0100

    Added styling of Link to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  font-size: 18px;
+`;
 
 export default function HomePage() {
   return (
     <div>
-      <Link href="/volumes">Go to all volumes</Link>
+      <h1>Welcome to Next.js!</h1>
+      <br/>
+      <StyledLink href="/volumes">Go to all volumes</StyledLink>
     </div>
   );
 }
