23eb4a3f42494e5ef6140640d0309d652bc2da2a
Catherine <catherine.dieval@hotmail.fr>
2024-12-17 16:43:41 +0100

    Added br elements to index.js

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import StyledLink from "@/Components/StyledLink";
 
 export default function HomePage() {
   return (
-    <div>
+    <>
+      <br/><br/>
       <h1>Welcome to Next.js!</h1>
       <br/>
       <StyledLink href="/volumes">Go to all volumes</StyledLink>
-    </div>
+    </>
   );
 }
